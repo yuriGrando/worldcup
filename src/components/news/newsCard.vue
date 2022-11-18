63e9b34f966b69d56ebd9a01d004ebac5eef7f03
yuriGrando <yv.grando@gmail.com>
2022-11-17 21:34:54 -0300

    index

---
+++ src/components/news/newsCard.vue
@@ -0,0 +1,66 @@
+<template>
+    <q-card
+        class="fit"
+        style="font-family: Roboto,serif"
+        bordered
+    >
+        <q-img
+            src="https://s2.glbimg.com/nOcL9h14s6Yr9tHP3Bm4dSXBw3o=/0x0:1024x699/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_bc8228b6673f488aa253bbcb03c80ec5/internal_photos/bs/2019/R/l/1vB2GJQIeOPqwcfgrDVg/mane.jpg"
+        />
+
+        <q-card-section>
+            <div
+                style="font-size: 0.7rem"
+                class="q-mt-sm q-mb-xs text-grey-6"
+            >
+                17/11/2022
+            </div>
+
+            <div
+                style="font-size: 0.7rem"
+                class="q-mt-sm q-mb-xs text-bold"
+            >
+                Sadio Mané, de Senegal, está fora da Copa do Mundo do Catar
+            </div>
+            <q-btn
+                color="grey"
+                round
+                flat
+                dense
+                :icon="expanded ? 'keyboard_arrow_up' : 'keyboard_arrow_down'"
+                @click="expanded = !expanded"
+            />
+        </q-card-section>
+
+        <q-slide-transition>
+            <div v-show="expanded">
+                <q-separator />
+                <q-card-section class="text-subitle2">
+                    sdasdfgaddsa ds
+                    afasdsada
+                    fsafsadfsadasdsad
+                    sadsadasd
+                </q-card-section>
+            </div>
+        </q-slide-transition>
+    </q-card>
+</template>
+
+<script>
+export default {
+    name: "newsCard",
+    props:{
+        news: Object,
+    },
+
+    data(){
+        return{
+            expanded: false,
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -1,6 +1,125 @@
 <template>
-    <q-page class="flex flex-center fontQatar">
-        <div style="width: 80%">
+    <div class="flex flex-center fontQatar" style="height: calc(100vh - 150px)">
+        <div class="row fit">
+            <div class="col-3">
+                <q-scroll-area style="height: calc(100vh - 150px)">
+
+                    <div class="q-pa-md" v-for="c in 10" :key="c">
+                        <news-card/>
+                    </div>
+
+                </q-scroll-area>
+
+
+            </div>
+            <div class="col-8 flex column q-pa-md ">
+
+                <div class="col-7 row">
+
+                    <div class="col-6">
+
+                        <div class="text-center">
+                            <img
+                                alt="App da Copa do Mundo"
+                                src="https://upload.wikimedia.org/wikipedia/pt/thumb/e/e3/2022_FIFA_World_Cup.svg/1200px-2022_FIFA_World_Cup.svg.png"
+                                style=" height: 150px"
+                            >
+                        </div>
+
+                        <div class="">
+                            <div class="text-h6 text-center q-my-md">
+                                Começa em:
+                            </div>
+                            <div class="flex flex-center text-primary">
+                                <q-item class="flex flex-center text-center">
+                                    <q-item-section>
+                                        <span class="timer">{{ d }}</span>
+                                        <span class="words text-subtitle1 q-pt-md">dias</span>
+                                    </q-item-section>
+                                </q-item>
+
+                                <q-item class="flex flex-center text-center">
+                                    <q-item-section>
+                                        <span class="timer">{{ h }}</span>
+                                        <span class="words text-subtitle1 q-pt-md">horas</span>
+                                    </q-item-section>
+                                </q-item>
+
+                                <q-item class="flex flex-center text-center">
+                                    <q-item-section>
+                                        <span class="timer">{{ m }}</span>
+                                        <span class="words text-subtitle1 q-pt-md">minutos</span>
+                                    </q-item-section>
+                                </q-item>
+
+                            </div>
+                        </div>
+
+                    </div>
+
+                    <div class="col-6">
+                        <div class="flex flex-center">
+                            <div class="q-pa-xs row flex flex-center fit" style="background-color: #881335">
+                                <div class="col-12 text-white text-center" style="font-size: 0.7rem">
+                                    {{ "10/10/10" }}
+                                </div>
+
+                                <div class="col-5 items-center q-px-sm justify-between flex">
+
+
+                                    <div class="fontQatar text-white" style="font-size:1rem">
+                                        {{ "BRASIL" }}
+                                    </div>
+                                    <div>
+                                        <q-avatar color="white" size="35px"></q-avatar>
+                                    </div>
+                                </div>
+
+
+                                <div class="col-2 flex flex-center">
+                                    <div class="fontQatar text-center text-white" style="font-size:1.3rem ">
+                                        {{ "0" }}
+                                    </div>
+                                    <div class="fontQatar q-px-xs text-center text-white" style="font-size:1rem ">
+                                        X
+                                    </div>
+                                    <div class="fontQatar text-center text-white" style="font-size:1.3rem ">
+                                        {{ "0" }}
+                                    </div>
+                                </div>
+
+
+                                <div class="col-5 items-center q-px-sm justify-between flex">
+                                    <div>
+                                        <q-avatar size="35px" color="white"></q-avatar>
+                                    </div>
+                                    <div class="fontQatar text-white" style="font-size:1rem">
+                                        {{ "BRASIL" }}
+                                    </div>
+                                </div>
+
+                                <div class="col-12 text-white text-center" style="font-size: 0.7rem">
+                                    <q-badge color='primary'> {{ 'Finalizada' }} </q-badge>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="col items-end">
+                    <table-group
+                        :teams="tableTest"
+                        :group="'A'"
+                    />
+
+                </div>
+
+
+            </div>
+        </div>
+        <!--<div style="width: 80%">
             <div class="row flex flex-center">
                 <div class="flex flex-center col" style="max-width: 30%">
                     <img
@@ -49,7 +168,7 @@
 
                             <div class="col-5 items-center q-px-sm justify-between flex">
 
-                                <!-- TIME DA CASA -->
+
                                 <div class="fontQatar text-white" style="font-size:1rem">
                                     {{ "BRASIL" }}
                                 </div>
@@ -58,7 +177,7 @@
                                 </div>
                             </div>
 
-                            <!-- PLACAR -->
+
                             <div class="col-2 flex flex-center">
                                 <div class="fontQatar text-center text-white" style="font-size:1.3rem ">
                                     {{ "0" }}
@@ -71,7 +190,7 @@
                                 </div>
                             </div>
 
-                            <!-- TIME DA VISITANTE -->
+
                             <div class="col-5 items-center q-px-sm justify-between flex">
                                 <div>
                                     <q-avatar size="35px" color="white"></q-avatar>
@@ -88,32 +207,40 @@
                     </div>
                 </div>
             </div>
-        </div>
-    </q-page>
+        </div> -->
+    </div>
 </template>
 
 <script>
 import {defineComponent} from 'vue'
 import {mapActions, mapMutations} from "vuex";
 import TableGroup from "components/table/tableGroup";
-import {SET_TOKEN} from "src/store/worldCup/mutations";
+import NewsCard from "components/news/newsCard";
 
 export default defineComponent({
     name: 'IndexPage',
-    components: {TableGroup},
+    components: {NewsCard, TableGroup},
     data() {
         return {
             d: '00',
             h: '00',
             m: '00',
             s: '00',
+
+            // ===== TABLE GROUP =====
+            table : [],
+            groupSelect: 0,
+            tableTest: []
         }
     },
 
-    mounted() {
-        this.getInfo()
-        setInterval(this.updateTimer, 1000)
+    computed: {
+        groupFilter: function (){
+            console.log(this.table)
+            return this.table
+        }
     },
+
 
     watch: {
         d() {
@@ -128,34 +255,14 @@ export default defineComponent({
 
     methods: {
         ...mapActions('worldCup', ['ActionDoLogin', 'ActionGetStandings', 'ActionGetMatch', 'ActionGetTeam']),
-        ...mapMutations('worldCup', ['SET_STANDINGS', "SET_MATCH", "SET_TEAM"]),
-        getInfo() {
-            this.ActionGetTeam().then((res) => {
-                localStorage.setItem('team', res.data.data)
-                this.SET_TEAM(res.data.data)
-                console.log(res.data.data)
-            });
+
+        // ======= REQUISIÇÃO DA TABELA =========
+        getTable() {
             this.ActionGetStandings().then((res) => {
-                this.SET_STANDINGS(res.data.data)
-                localStorage.setItem('standings', res.data.data)
-                console.log(res.data.data)
-            });
-            this.ActionGetMatch().then((res) => {
-                localStorage.setItem('match', res.data.data)
-                this.SET_MATCH(res.data.data)
+                this.table = res.data.data;
+                this.tableTest = res.data.data[0].teams
             })
         },
-
-        // getStarted(){
-        //   this.ActionDoLogin({
-        //     email: "yv.grando@gmail.com",
-        //     password: "1o*18*v9*"
-        //   }).then((res)=>{
-        //     SET_TOKEN(res.data.data.token)
-        //     console.log(res)
-        //   })
-        //
-        // }
 
         updateTimer() {
             let future = Date.parse("November 20, 2022 13:00:00");
@@ -173,6 +280,13 @@ export default defineComponent({
             this.s = secs - min * 60;
         },
     },
+
+    mounted() {
+
+        this.getTable();
+
+        setInterval(this.updateTimer, 1000)
+    },
 })
 
 </script>
@@ -180,14 +294,12 @@ export default defineComponent({
 <style>
 
 .timer {
-    color: white;
     display: block;
-    font-size: 4em;
+    font-size: 3em;
     text-shadow: 0 0 5px #ffffff;
 }
 
 .words {
-    color: white;
     display: block;
     font-size: 1.5em;
     text-shadow: 0 0 10px #ffffff;
